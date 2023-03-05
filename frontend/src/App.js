414c51db13c0f65d15401c71b55f9f2f906fddef
Brandon Tran <tranbrandon1233@gmail.com>
2023-03-05 15:16:05 -0800

    Front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,38 @@
 import './App.css';
 import {useState, React, useEffect} from 'react'
 import axios from 'axios';
-
 const URL = "http://localhost:3002";
 
 function App(props) {
   
-const [message, setMessage] = useState("")
-const [posts, setPosts] = useState([{
-  user: "name",
-  content: "message",
-  timestamp: Date.now(),
-  num_likes: "3"
-}])
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [description, setDescription] = useState("");
+  const [num_riders, setRiders] = useState("");
+  const [locationTo, setLocationTo] = useState("");
+  const [username, setUsername] = useState("");
+  const [locationFrom, setLocationFrom] = useState("");
+const [posts, setPosts] = useState([
+  {
+
+  }
+]);
 function Posts(props){
   return(
     <div className='Posts'>
         <h3> {props.user} </h3>
-        <p> {props.content} - Time: {props.timestamp} - Likes: {props.num_likes}</p>
-        <p>{props.timestamp}</p>
+        <p> Date: {props.date} - Time: {props.timestamp} </p>
+        <p>From: {props.locationFrom} - To: {props.locationTo}</p>
+        <p>Number of Riders: {props.num_riders}</p>
+        <p>Description: {props.description}</p>
     </div>
   );
 }
-/*
-    
-
-function CreatePost(props)  {
- <div>
-   <input className = "CreatePost" type="text" value={props.newPost} onChange={(e)=>props.setNewPost(e.target.value)} placeholder="type"></input>
-   
-   <button onClick={()=>{props.addPost();}}>Post</button>
- </div>
-
-}
-
-
-
-
 
 
   const getPosts = () => {
     axios.get(URL + "/feed")
       .then(response => {
-        setMessage(response.data);
         setPosts(response.data);
       })
       .catch(console.error)
@@ -50,27 +40,40 @@ function CreatePost(props)  {
   
   
   function addPost(){
-    if(message === ""){
-      alert("Please enter a message");
+    if(username === "" || date === "" || time === "" || locationFrom === "" || locationTo === "" || num_riders === "" ){
+      alert("Please provide input for all of the required fields.");
     }
     axios.post(URL + "/feed/new", {
-      content: message,
-      user: "name"
+      time: time,
+      username: username,
+      date: date,
+      description: description,
+      num_riders: num_riders,
+      locationFrom: locationFrom,
+      locationTo: locationTo
     }).then(response => {
       console.log(response);
       getPosts();
     })
     .catch(console.error);
-    setMessage("");
   }
-  */
- // useEffect(() => {}, [message]);
+  
+  useEffect(() => {}, [date, time, description, num_riders, locationFrom, locationTo, username]);
   return (
     <div>
       <h1>EasyRide</h1>
+      <input  type="text" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder="Name:"></input>
+      <input  type="text" value={date} onChange={(e)=>setDate(e.target.value)} placeholder="Date:"></input>
+      <input  type="text" value={time} onChange={(e)=>setTime(e.target.value)} placeholder="Time:"></input>
+      <input  type="text" value={locationFrom} onChange={(e)=>setLocationFrom(e.target.value)} placeholder="Location from:"></input>
+      <input  type="text" value={locationTo} onChange={(e)=>setLocationTo(e.target.value)} placeholder="Location to:"></input>
+      <input  type="text" value={num_riders} onChange={(e)=>setRiders(e.target.value)} placeholder="Number of riders:"></input>
+      <input  type="text" value={description} onChange={(e)=>setDescription(e.target.value)} placeholder="Description:"></input>
+   <button onClick={()=>{addPost();}}>Post</button>
+   
       {posts.map((post, i) => 
         <div key={i}>
-          <Posts user={post.user} content={post.content} timestamp={post.timestamp} num_likes={post.num_likes} > </Posts>
+          <Ride user={username} locationFrom={locationFrom} time={time} locationTo={locationTo} date={date} num_riders={num_riders} description={description}/>
         </div>        
       ).reverse()}
     </div>
