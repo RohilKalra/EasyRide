10be56235ea99dec90988a1c055ebed9be84718c
Brandon Tran <tranbrandon1233@gmail.com>
2023-03-05 11:22:37 -0800

    backend from stackschool

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, React, useEffect} from 'react'
+import axios from 'axios';
 
-function App() {
+const URL = "http://localhost:3001";
+
+function App(props) {
+  
+const [message, setMessage] = useState("")
+const [posts, setPosts] = useState([])
+
+    
+
+function CreatePost(props)  {
+ <div>
+   <input className = "CreatePost" type="text" value={props.newPost} onChange={(e)=>props.setNewPost(e.target.value)} placeholder="type"></input>
+   
+   <button onClick={()=>{props.addPost();}}>Post</button>
+ </div>
+
+}
+
+
+function Posts(props){
+  return(
+    <div className='Posts'>
+        <h3> {props.user} </h3>
+        <p> {props.content} - Time: {props.timestamp} - Likes: {props.num_likes}</p>
+        <p>{props.timestamp}</p>
+    </div>
+  );
+}
+
+
+  const getPosts = () => {
+    axios.get(URL + "/feed")
+      .then(response => {
+        setMessage(response.data);
+        setPosts(response.data);
+      })
+      .catch(console.error)
+  }
+  
+  
+  function addPost(){
+    if(message === ""){
+      alert("Please enter a message");
+    }
+    axios.post(URL + "/feed/new", {
+      content: message,
+      user: "name"
+    }).then(response => {
+      console.log(response);
+      getPosts();
+    })
+    .catch(console.error);
+    setMessage("");
+  }
+  
+  useEffect(() => {}, [message]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CreatePost newPost={message} setNewPost={setMessage} addPost={addPost}/>
+      {posts.map((post, i) => 
+        <div key={i}>
+          <Posts user={post.user} content={post.content} timestamp={post.timestamp} > </Posts>
+        </div>        
+      )}.reverse();
     </div>
   );
 }
