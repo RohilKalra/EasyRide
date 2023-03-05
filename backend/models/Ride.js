6f2dc8b129d647a6ec436fba8223add0e86567a9
pravirchugh <pravirchugh@yahoo.com>
2023-03-05 12:10:03 -0800

    Added API Endpoints for GET and POST requests, updated server.js

---
+++ backend/models/Ride.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RideSchema = new Schema({
+    date: {
+        type: String,
+        default: "03/05/2023",
+    },
+    time: {
+        type: String,
+        default: "12:00"
+        // military time
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    locationFrom: {
+        type: String,
+        required: true
+    },
+    locationTo: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: ""
+    }
+
+});
+
+const Ride = mongoose.model("Ride", RideSchema);
+module.exports = Ride;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+// express.js, will make working with apis easier (framework)
+const express = require('express');
+
 // require mongoose library (use require for node.js, import for other javascript situations)
 const mongoose = require('mongoose');
 
+const cors = require('cors');
+
 // please replace <username> and <password>!
-connection = "mongodb+srv://<username>:<password>@easyrideone.0gqx4ay.mongodb.net/?retryWrites=true&w=majority";
+connection = "mongodb+srv://demo:1234@easyrideone.0gqx4ay.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.set('strictQuery', false);
 
@@ -16,3 +21,43 @@ mongoose
     .then(() => console.log("Connected to MongoDB")) 
     .catch(console.error)
 ;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const app = express();
+app.use(express.json()); // when I'm receiving information, format it in the JSON format.
+app.use(cors());
+
+app.listen(8080, () => console.log('Server listening on port 8080')); // first parameter is port, second parameter is what to do after setup.
+
+// mongoose specific
+
+// where we connect
+
+// add a post
+const Ride = require('./models/Ride'); // import our Ride schema
+
+// using express! -> gets all posts and returns all 
+app.get('/feed', async (req, res) => {
+    const feed = await Ride.find(); // look at the collection that matches the post schema and get all of the results
+    
+    res.json(feed); // once we are done, plug it into feed and store it into res with the JSON format
+    
+});
+// req res is express syntax!
+app.post('/feed/new', (req, res) => {
+    const ride = new Ride({
+        time: req.body.time,
+        username: req.body.username,
+        locationFrom: req.body.locationFrom,
+        locationTo: req.body.locationTo,
+        description: req.body.description,
+    });
+    ride.save();
+    res.json(ride);
+});
+
+app.delete('/feed/delete/:id', async(req, res) => {
+    const result = await Ride.findByIdAndDelete(req.params.id);
+    res.json(result);
+});
